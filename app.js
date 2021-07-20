dc10231a6125a8baf1f968dd8f5ee55ca33c7ed9
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-07-20 21:30:10 +0530

    Added environment variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -21,8 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-var secret = "ThisismySecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
